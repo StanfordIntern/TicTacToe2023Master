5181e6d941a190e2b136fdfde0e132bec829ff7b
Idea24 <117238835+StanfordIntern@users.noreply.github.com>
2023-02-06 01:35:51 +0000

    Created columns and diagonal lists, coordinate labeling, and input for X-Player

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
 #Creates top, middle, and bottom
-topRow = ["-", "-", "-"]
-middleRow = ["-", "-", "-"]
-bottomRow = ["-", "-", "-"]
+topRow = ["1","-", "-", "-"]
+middleRow = ["2","-", "-", "-"]
+bottomRow = ["3","-", "-", "-"]
+
+columnA = [topRow[1], middleRow[1], bottomRow[1]]
+columnB = [topRow[2], middleRow[2], bottomRow[2]]
+columnC = [topRow[3], middleRow[3], bottomRow[3]]
+
+diagonalUpToDown = [topRow[1], middleRow[2], bottomRow[3]]
+diagonalDownToUp = [topRow[3], middleRow[2], bottomRow[1]]
 
 print(topRow)
 print(middleRow)
 print(bottomRow)
+print("       a", "   b", "   c")
+
+moveX = input("X-Player Turn:")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
